d70db96fff2eeee21d8fc6bc95bf9c93e68362b5
IIUJ-ArielZakrzewski <ariel.zakrzewski@gmail.com>
2012-12-19 09:54:04 +0100

    poprawa nieudanego merga i002_z010

--- museSort/museSort/obiektXML.cs
+++ museSort/museSort/obiektXML.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,48 +9,57 @@ namespace museSort
 {
     class obiektXML
     {
-        String sciezka;
+        String sciezka;//przechowuje scieżkę do pliku XML
         XmlDocument plikXML;
+        String schemat;
 
-        public obiektXML()//otwieramy folder do tworzenia pliku XML
+        public obiektXML(String sc)//otwieramy folder do tworzenia pliku XML
         {
             sciezka = "";
+            schemat = sc;
             plikXML = new XmlDocument();
         }
 
-        public obiektXML(String path)//wiążemy istniejący plik XML w folderze ze ścieżki z argumentu z obiektem
+        public obiektXML(String path, String sc)//wiążemy istniejący plik XML w folderze ze ścieżki z argumentu z obiektem
         {
             sciezka = path;
+            schemat = sc;
             plikXML = new XmlDocument();
-            plikXML.Load(sciezka);
+            String p = sciezka + "\\" + "struktura_logiczna.xml";
+            plikXML.Load(p);
 
         }
 
-        public void utworz_nowy_xml(String path)//tworzymy nowy plik XML
+        public void generujXML(String path)//tworzymy nowy plik XML
         {
-            sciezka = path + "\\" + "struktura_logiczna.xml";
-            if (System.IO.File.Exists(sciezka))
+            sciezka = path;
+            String p = sciezka + "\\" + "struktura_logiczna.xml";//sciezka z plikiem
+            if (System.IO.File.Exists(p))
             {
-                System.IO.File.Delete(sciezka);
+                System.IO.File.Delete(p);
             }
+            plikXML = new XmlDocument();
             XmlDeclaration dec = plikXML.CreateXmlDeclaration("1.0", "UTF-8", null);
             plikXML.AppendChild(dec);
-            String[] foldery = path.Split('\\');
-            XmlElement main = plikXML.CreateElement("lista_folderow");
-            generuj_elementy(path, main);
+            XmlElement main = plikXML.CreateElement("body");
+            XmlElement sc = plikXML.CreateElement("schemat");
+            XmlText wartosc = plikXML.CreateTextNode(schemat);
+            sc.AppendChild(wartosc);
+            main.AppendChild(sc);
+            generuj_elementy(path, main, plikXML);
             plikXML.AppendChild(main);
-            plikXML.Save(sciezka);
+            plikXML.Save(p);
 
         }
 
-        public void generuj_elementy(String path, XmlElement parent)
+        public void generuj_elementy(String path, XmlElement parent, XmlDocument doc)
         {
             XmlElement aktualny;
             String[] foldery = path.Split('\\'); //pobieram nazwę aktualnego folderu
 
-            aktualny = plikXML.CreateElement("folder");
-            XmlElement name = plikXML.CreateElement("nazwa");
-            XmlText wartosc = plikXML.CreateTextNode(foldery[foldery.Length - 1]);
+            aktualny = doc.CreateElement("folder");
+            XmlElement name = doc.CreateElement("nazwa");
+            XmlText wartosc = doc.CreateTextNode(foldery[foldery.Length - 1]);
             name.AppendChild(wartosc);
             aktualny.AppendChild(name);
 
@@ -58,23 +67,60 @@ namespace museSort
             Array.Sort(foldery);
             foreach (string folderName in foldery)
             {
-                generuj_elementy(folderName, aktualny);
+                generuj_elementy(folderName, aktualny, doc);
             }
 
             string[] pliki;
             pliki = Directory.GetFiles(path); //tworzę listę plików
             foreach (string fileName in pliki)
             {
-                XmlElement plik = plikXML.CreateElement("plik");
-                name = plikXML.CreateElement("nazwa");
-                foldery = fileName.Split('\\');
-                wartosc = plikXML.CreateTextNode(foldery[foldery.Length - 1]);
-                name.AppendChild(wartosc);
-                plik.AppendChild(name);
-                aktualny.AppendChild(plik);
+                if (!(System.IO.Path.GetExtension(fileName).Substring(1) == "xml"))
+                {
+                    XmlElement plik = doc.CreateElement("plik");
+                    name = doc.CreateElement("nazwa");
+                    foldery = fileName.Split('\\');
+                    wartosc = doc.CreateTextNode(foldery[foldery.Length - 1]);
+                    name.AppendChild(wartosc);
+                    plik.AppendChild(name);
+                    aktualny.AppendChild(plik);
+                }
             }
 
             parent.AppendChild(aktualny);
         }
+
+        public Boolean analizuj()
+        {
+            if (sciezka == "")
+            {
+                return true;
+            }
+            XmlDocument tempXML = new XmlDocument();
+            XmlDeclaration dec = tempXML.CreateXmlDeclaration("1.0", "UTF-8", null);
+            tempXML.AppendChild(dec);
+            XmlElement main = tempXML.CreateElement("lista_folderow");
+            generuj_elementy(sciezka, main, tempXML);
+            tempXML.AppendChild(main);
+            String p = sciezka + "\\" + "temp.xml";
+            String q = sciezka + "\\" + "struktura_logiczna.xml";
+            tempXML.Save(p);
+            if (System.IO.File.ReadAllText(p) == System.IO.File.ReadAllText(q))
+            {
+                System.IO.File.Delete(p);
+                return true;
+            }
+            else
+            {
+                System.IO.File.Delete(p);
+                return false;
+            }
+
+        }
+
+        public void aktualizuj()
+        {
+            generujXML(sciezka);
+        }
+
     }
 }
