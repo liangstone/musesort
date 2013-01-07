940fe56a5b468b567d522c8a6910445980e1bc09
IIUJ-ArielZakrzewski <ariel.zakrzewski@gmail.com>
2013-01-07 20:52:56 +0100

    i003_z007

--- museSort/museSort/MainGUI.Designer.cs
+++ museSort/museSort/MainGUI.Designer.cs
@@ -47,18 +47,18 @@
             this.format = new System.Windows.Forms.ComboBox();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.NazwaPliku = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.Tytul = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.wykonawca = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.Album = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.Rok = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.Gatunki = new System.Windows.Forms.ListBox();
+            this.label9 = new System.Windows.Forms.Label();
             this.NumerSciezki = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
@@ -116,7 +116,7 @@
             // Modyfikuj
             // 
             this.Modyfikuj.BackColor = System.Drawing.Color.SkyBlue;
-            this.Modyfikuj.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Modyfikuj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Modyfikuj.Location = new System.Drawing.Point(97, 36);
             this.Modyfikuj.Name = "Modyfikuj";
             this.Modyfikuj.Size = new System.Drawing.Size(145, 51);
@@ -128,7 +128,7 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.DodgerBlue;
-            this.button2.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(286, 36);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(129, 51);
@@ -147,7 +147,7 @@
             // Eksport
             // 
             this.Eksport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.Eksport.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Eksport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Eksport.Location = new System.Drawing.Point(1000, 585);
             this.Eksport.Name = "Eksport";
             this.Eksport.Size = new System.Drawing.Size(173, 29);
@@ -185,11 +185,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 18);
+            this.label1.Size = new System.Drawing.Size(127, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Schemat zapisu:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -197,7 +197,7 @@
             // schematy
             // 
             this.schematy.FormattingEnabled = true;
-            this.schematy.Location = new System.Drawing.Point(120, 3);
+            this.schematy.Location = new System.Drawing.Point(136, 3);
             this.schematy.Name = "schematy";
             this.schematy.Size = new System.Drawing.Size(121, 21);
             this.schematy.TabIndex = 1;
@@ -205,18 +205,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label2.Location = new System.Drawing.Point(247, 0);
+            this.label2.Location = new System.Drawing.Point(263, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(103, 18);
+            this.label2.Size = new System.Drawing.Size(107, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Format plików";
             // 
             // format
             // 
             this.format.FormattingEnabled = true;
-            this.format.Location = new System.Drawing.Point(356, 3);
+            this.format.Location = new System.Drawing.Point(376, 3);
             this.format.Name = "format";
             this.format.Size = new System.Drawing.Size(121, 21);
             this.format.TabIndex = 3;
@@ -246,132 +246,132 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.label3.Location = new System.Drawing.Point(3, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 18);
+            this.label3.Size = new System.Drawing.Size(97, 20);
             this.label3.TabIndex = 0;
             this.label3.Text = "Nazwa pliku:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label4.Location = new System.Drawing.Point(405, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 18);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Tytuł:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label5.Location = new System.Drawing.Point(3, 26);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(88, 18);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Wykonawca:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label6.Location = new System.Drawing.Point(405, 26);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(56, 18);
-            this.label6.TabIndex = 3;
-            this.label6.Text = "Album:";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label7.Location = new System.Drawing.Point(3, 49);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(96, 18);
-            this.label7.TabIndex = 4;
-            this.label7.Text = "Rok wydania:";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label8.Location = new System.Drawing.Point(405, 49);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(65, 18);
-            this.label8.TabIndex = 5;
-            this.label8.Text = "Gatunki:";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label9.Location = new System.Drawing.Point(3, 72);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(100, 18);
-            this.label9.TabIndex = 6;
-            this.label9.Text = "Numer ścieżki";
             // 
             // NazwaPliku
             // 
             this.NazwaPliku.FormattingEnabled = true;
-            this.NazwaPliku.Location = new System.Drawing.Point(102, 3);
+            this.NazwaPliku.Location = new System.Drawing.Point(106, 3);
             this.NazwaPliku.Name = "NazwaPliku";
             this.NazwaPliku.Size = new System.Drawing.Size(297, 17);
             this.NazwaPliku.TabIndex = 7;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Location = new System.Drawing.Point(409, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Tytuł:";
+            // 
             // Tytul
             // 
             this.Tytul.FormattingEnabled = true;
-            this.Tytul.Location = new System.Drawing.Point(456, 3);
+            this.Tytul.Location = new System.Drawing.Point(462, 3);
             this.Tytul.Name = "Tytul";
             this.Tytul.Size = new System.Drawing.Size(402, 17);
             this.Tytul.TabIndex = 8;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label5.Location = new System.Drawing.Point(3, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Wykonawca:";
+            // 
             // wykonawca
             // 
             this.wykonawca.FormattingEnabled = true;
-            this.wykonawca.Location = new System.Drawing.Point(97, 29);
+            this.wykonawca.Location = new System.Drawing.Point(107, 26);
             this.wykonawca.Name = "wykonawca";
             this.wykonawca.Size = new System.Drawing.Size(302, 17);
             this.wykonawca.TabIndex = 9;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label6.Location = new System.Drawing.Point(415, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 20);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Album:";
+            // 
             // Album
             // 
             this.Album.FormattingEnabled = true;
-            this.Album.Location = new System.Drawing.Point(467, 29);
+            this.Album.Location = new System.Drawing.Point(479, 26);
             this.Album.Name = "Album";
             this.Album.Size = new System.Drawing.Size(391, 17);
             this.Album.TabIndex = 10;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label7.Location = new System.Drawing.Point(3, 46);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 20);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Rok wydania:";
+            // 
             // Rok
             // 
             this.Rok.FormattingEnabled = true;
-            this.Rok.Location = new System.Drawing.Point(105, 52);
+            this.Rok.Location = new System.Drawing.Point(112, 49);
             this.Rok.Name = "Rok";
             this.Rok.Size = new System.Drawing.Size(294, 17);
             this.Rok.TabIndex = 11;
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label8.Location = new System.Drawing.Point(412, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 20);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Gatunki:";
+            // 
             // Gatunki
             // 
             this.Gatunki.FormattingEnabled = true;
-            this.Gatunki.Location = new System.Drawing.Point(476, 52);
+            this.Gatunki.Location = new System.Drawing.Point(487, 49);
             this.Gatunki.Name = "Gatunki";
             this.Gatunki.Size = new System.Drawing.Size(382, 17);
             this.Gatunki.TabIndex = 12;
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label9.Location = new System.Drawing.Point(3, 69);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(107, 20);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Numer ścieżki";
+            // 
             // NumerSciezki
             // 
             this.NumerSciezki.FormattingEnabled = true;
-            this.NumerSciezki.Location = new System.Drawing.Point(109, 75);
+            this.NumerSciezki.Location = new System.Drawing.Point(116, 72);
             this.NumerSciezki.Name = "NumerSciezki";
             this.NumerSciezki.Size = new System.Drawing.Size(290, 17);
             this.NumerSciezki.TabIndex = 13;
