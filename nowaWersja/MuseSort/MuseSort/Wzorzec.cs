ad113047d5207de6d77586d87a8f9c8b5ec7acea
IIUJ-KarolinaDrabowska <kdrabowska@gmail.com>
2013-03-15 18:15:36 +0100

    i004_z002

--- nowaWersja/MuseSort/MuseSort/Wzorzec.cs
+++ nowaWersja/MuseSort/MuseSort/Wzorzec.cs
@@ -32,11 +32,6 @@ namespace MuseSort
             return rezultat;
         }
 
-        public void wczytajWzorceZPliku(String path)
-        {
-            //Wczytywanie listy wzorców z pliku XML do obiektów klasy
-            //W zmiennej path zawarta jest ścieżka do pliku
-        }
 
         //######################################METODY POMOCNICZE KLASY######################################
     }
