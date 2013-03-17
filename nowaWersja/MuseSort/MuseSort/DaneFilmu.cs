3b0ecd842f007256a8370bc2efd03bc4c6bd8185
IIUJ-ArielZakrzewski <ariel.zakrzewski@gmail.com>
2013-03-17 18:59:20 +0100

    i004_z010

---
+++ nowaWersja/MuseSort/MuseSort/DaneFilmu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MuseSort
+{
+    class DaneFilmu
+    {
+        //Implementacja klasy zależy od biblioteki z jakiej będziemy korzystali
+
+        //Sprawdzanie, czy mamy wszystko, czego nam trzeba
+        public Boolean czyDaneWypelnione()
+        {
+            
+            return false;
+        }
+    }
+}
