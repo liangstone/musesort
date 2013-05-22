b6f754f5b8e3a02a8abd5f9746583443fa6058db
IIUJ-KarolinaDrabowska <kdrabowska@gmail.com>
2013-05-22 13:42:36 +0200

    i007_z009

--- nowaWersja/MuseSort/MuseSort/UtworzUstawienia.cs
+++ nowaWersja/MuseSort/MuseSort/UtworzUstawienia.cs
@@ -11,6 +11,7 @@ namespace MuseSort
 {
     public partial class UtworzUstawienia : Form
     {
+
         String visiblePanel;
         List<IComponent> rozszerzenia;
         public UtworzUstawienia()
